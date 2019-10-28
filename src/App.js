823265004b88e5782cfe24b2f2a7fab4d95f635a
Jack Mittelmeier <jackmittelmeier@email.arizona.edu>
2019-10-27 20:46:55 -0700

    update about page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import {isMobile} from 'react-device-detect';
 import './App.css';
 import MenuIcon from '@material-ui/icons/Menu'
 
@@ -99,7 +100,12 @@ function AppRouter({ pages }) {
   console.log(selectedPage)
 
   return (
-    <Container>
+    <Container
+      style = {{
+        alignItems : 'flex-start',
+        height: 'auto'
+      }}
+    >
       {(pages && selectedPage) && React.createElement(pages[selectedPage].component)}
       <MobileMenu pageNames={pageNames} selectedPage={selectedPage} />
     </Container>
@@ -107,11 +113,54 @@ function AppRouter({ pages }) {
 
 }
 
+function Header ({ value, height }) {
+  return(
+    <Container
+      style = {{
+        height: height || 40,
+        fontSize: isMobile ? '24px' : '34px'
+      }}
+    >
+      {value}
+    </Container>
+  )
+}
+
 function HomePage({ }) {
 
   return (
-    <Container>
-      HOME
+    <Container
+      style = {{
+        flexFlow: 'column',
+        height: 'auto',
+        justifyContent: 'flex-start'
+      }}
+    >
+      <img
+        style = {{
+          width: '100vw',
+          height: '100vh',
+          objectFit: 'cover'
+        }}
+         
+        src="/images/placeholder.png" 
+      />
+
+
+      <Header value = "About" />
+      <Container
+        style = {{
+          fontFamily: "'Catamaran', sans-serif",
+          fontSize: isMobile ? '16px' : '24px',
+          height: '50vh',
+          width: '60%'
+        }}
+      >
+      In a time where individuality is appreciated more than ever, it is vitally important to express yourself to the fullest extent. 
+      Kowalski aims to help passionate and unique individuals live their truth. Follow your passion and make those special moments count. 
+      Take full advantage of the opportunities that this world has to offer. Make your passion your lifestyle.
+
+      </Container>
     </Container>
   )
 }
@@ -155,7 +204,12 @@ function App() {
   }
 
   return (
-    <Container>
+    <Container
+      style = {{
+        alignItems : 'flex-start',
+        height: 'auto'
+      }}
+    >
       <AppRouter pages={pages} />
     </Container>
   );
